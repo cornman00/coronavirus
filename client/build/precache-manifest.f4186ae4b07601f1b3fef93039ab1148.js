271fa0469aff63181767f704cd022337c6246b12
Seokhyun Kim <seokhyunkim@seokhyuns-mbp.attlocal.net>
2020-06-06 00:45:40 -0500

    first deploy

---
+++ client/build/precache-manifest.f4186ae4b07601f1b3fef93039ab1148.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0e0e064fa6473d0261bf9c00642751",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b88417b6fd58a31204f",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "11459f02267289f07e7f",
+    "url": "/static/css/main.35a16d20.chunk.css"
+  },
+  {
+    "revision": "8b88417b6fd58a31204f",
+    "url": "/static/js/2.5a076d98.chunk.js"
+  },
+  {
+    "revision": "a8fa0a685d8af2934ba8ade7768fed07",
+    "url": "/static/js/2.5a076d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11459f02267289f07e7f",
+    "url": "/static/js/main.69e944df.chunk.js"
+  },
+  {
+    "revision": "99a929c617f9377b0f84",
+    "url": "/static/js/runtime-main.7b753f39.js"
+  },
+  {
+    "revision": "836954d8eac8afd2f9e4444a13bc32ce",
+    "url": "/static/media/healthcare.836954d8.svg"
+  }
+]);
